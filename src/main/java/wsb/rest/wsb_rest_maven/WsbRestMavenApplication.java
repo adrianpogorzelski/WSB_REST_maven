64b54373dd72e4943c0d269bdc6cd86742a2cdbf
a.pogorzelski <pogorzelski.adrian@gmail.com>
2023-03-11 09:53:36 +0100

    First commit

---
+++ src/main/java/wsb/rest/wsb_rest_maven/WsbRestMavenApplication.java
@@ -0,0 +1,13 @@
+package wsb.rest.wsb_rest_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbRestMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbRestMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/rest/wsb_rest_maven/WsbRestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.rest.wsb_rest_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbRestMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
